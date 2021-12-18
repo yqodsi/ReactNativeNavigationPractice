77667e811d019eb1216629d89fd10a254e2cca54
Yqodsi <49367257+yqodsi@users.noreply.github.com>
2021-12-18 15:10:07 +0100

    fetch succeded, Navigation needs fix

--- components/ListRenderer.js
+++ components/ListRenderer.js
@@ -1,55 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { Button, Image, StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator
+} from "react-native";
 import NewsBox from "../components/NewsBox.js";
-import { createClient } from "pexels";
 
 function ListRenderer() {
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(false);
   const [data, setData] = useState([]);
-  const [img, setImage] = useState([]);
-
-  const getArticles = async () => {
-    try {
-      const response = await fetch(
-        "https://crypto-news6.p.rapidapi.com/news/coindesk",
-        {
-          method: "GET",
-          headers: {
-            "x-rapidapi-host": "crypto-news6.p.rapidapi.com",
-            "x-rapidapi-key":
-              "547997f5d3mshcbeae02fe149e60p1cdffejsnac2ccd39afca"
-          }
-        }
-      );
-      const json = await response.json();
-      setData(json);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const getImg = async () => {
-    try {
-      const response = await fetch(
-        "https://pixabay.com/api/?key=7582783-2ac15da3cf9b8e0efc9dd3615&q=crypto&image_type=photo",
-        {
-          method: "GET"
-        }
-      );
-      const json = await response.json();
-      setImage(json);
-      console.log(json.hits[0].pageURL);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
 
   useEffect(() => {
-    getImg();
+    const getArticles = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          "https://cryptonews-api.com/api/v1/category?section=general&items=50&token=7mp80huutbaii2hei2zbctx2zxpu5g5kmggxwkow",
+          {
+            method: "GET"
+          }
+        );
+        const json = await response.json();
+        setData(json);
+        setLoading(false);
+        console.log(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     getArticles();
   }, []);
 
@@ -59,23 +42,32 @@ function ListRenderer() {
     //   article="Russia Ukraine: Moscow lists demands for defusing Ukraine tensions"
     //   img_uri="https://picsum.photos/200/300"
     // />
-    <FlatList
-      data={data.slice(0, 15)}
-      initialNumToRender={10}
-      renderItem={({ item }) => (
-        <NewsBox
-          article={item.title}
-          url={item.url}
-          img_url={json.hits[0].pageURL}
-          source={item.source}
-        />
-      )}
-    />
+    !isLoading ? (
+      <FlatList
+        data={data.data}
+        initialNumToRender={10}
+        renderItem={({ item }) => (
+          <NewsBox
+            article={item.title}
+            url={item.news_url}
+            img_url={item.image_url}
+            source={item.source_name}
+          />
+        )}
+      />
+    ) : (
+      <View style={[styles.container]}>
+        <ActivityIndicator size="large" color="dodgerblue" />
+      </View>
+    )
   );
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  }
 });
 
 export default ListRenderer;
